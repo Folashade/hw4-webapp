36582645b468946baa4f98417461b61c935b4f09
Folashade Okunubi <foo@andrew.cmu.edu>
2013-02-17 03:44:45 -0500

    Added javascript file for chatroom, styled some of chat listings, added data file for user list

---
+++ data/users.js
@@ -0,0 +1,6 @@
+var users = { 
+	"1": "One",
+	"2": "Two",
+	"3": "Three",
+	"4": "Four"
+}
--- js/logic.js
+++ js/logic.js
@@ -1,1 +1,22 @@
-// to be filled
+window.onload = function(){
+	$(document).ready(function() {
+	// ^^this line necessary for all jQuery functioning 
+
+	// Debugging - console.logs run only when true
+	debug = true;
+
+	// Print function
+	var print = function(input){
+		if (debug === true){
+			console.log(input);
+		}
+	}
+	
+	var chatroom = function(){
+		var userlist = $('.user');
+		print(userlist[0].id);
+	}
+	
+	chatroom();
+	});	
+}
