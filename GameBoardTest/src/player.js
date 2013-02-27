e3ed0f5926533dc9d98495e39478628e2f065ea6
lju <lju@andrew.cmu.edu>
2013-02-27 07:25:23 -0800

    sprite beginning

--- GameBoardTest/src/player.js
+++ GameBoardTest/src/player.js
@@ -1,3 +1,8 @@
+var leftCount = 0;
+var rightCount = 0;
+var upCount = 0;
+var downCount = 0;
+
 function onKeyDown(event)
 {
 	var e = event.keyCode;
@@ -7,6 +12,17 @@ function onKeyDown(event)
 		if (GAME_ARRAY[PLAYER_POS.row-1][PLAYER_POS.col] !== 0)
 		{
 			PLAYER_POS.row--;
+			if ((upCount%2) === 0)
+			{
+				SERGEY = sergeyup1;
+			}
+			
+			else
+			{
+				SERGEY = sergeyup2;
+			}
+			
+			upCount++;
 		}
 	}
 		
@@ -15,6 +31,17 @@ function onKeyDown(event)
 		if (GAME_ARRAY[PLAYER_POS.row+1][PLAYER_POS.col] !== 0)
 		{
 			PLAYER_POS.row++;
+			if ((downCount%2) === 0)
+			{
+				SERGEY = sergeydown1;
+			}
+			
+			else
+			{
+				SERGEY = sergeydown2;
+			}
+			
+			downCount++;
 		}
 	}
 	
@@ -23,6 +50,17 @@ function onKeyDown(event)
 		if (GAME_ARRAY[PLAYER_POS.row][PLAYER_POS.col-1] !== 0)
 		{
 			PLAYER_POS.col--;
+			if ((leftCount%2) === 0)
+			{
+				SERGEY = sergeyleft1;
+			}
+			
+			else
+			{
+				SERGEY = sergeyleft2;
+			}
+			
+			leftCount++;
 		}
 	}
 
@@ -31,6 +69,17 @@ function onKeyDown(event)
 		if (GAME_ARRAY[PLAYER_POS.row][PLAYER_POS.col+1] !== 0)
 		{
 			PLAYER_POS.col++;
+			if ((rightCount%2) === 0)
+			{
+				SERGEY = sergeyright1;
+			}
+			
+			else
+			{
+				SERGEY = sergeyright2;
+			}
+			
+			rightCount++;
 		}
 	}
 }
