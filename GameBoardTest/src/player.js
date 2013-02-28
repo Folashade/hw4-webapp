a20853d1356ac40181ab9e9c241751bb847e36cc
lju <lju@andrew.cmu.edu>
2013-02-27 23:35:31 -0800

    the movement stuff

--- GameBoardTest/src/player.js
+++ GameBoardTest/src/player.js
@@ -5,86 +5,103 @@ var downCount = 0;
 
 function moveUp()
 {
-	if (GAME_ARRAY[PLAYER_POS.row-1][PLAYER_POS.col] === 1)
+	PLAYER_POS.row--;
+	if ((upCount%2) === 0)
 	{
-		PLAYER_POS.row--;
-		if ((upCount%2) === 0)
-		{
-			SERGEY = sergeyup1;
-		}
-		
-		else
-		{
-			SERGEY = sergeyup2;
-		}
-		
-		upCount++;
-		
-		setTimeout(moveUp, 1000);
+		SERGEY = sergeyup1;
 	}
+	
+	else
+	{
+		SERGEY = sergeyup2;
+	}
+	
+	upCount++;
 }
 		
 function moveDown()
 {
-	if (GAME_ARRAY[PLAYER_POS.row+1][PLAYER_POS.col] === 1)
+	PLAYER_POS.row++;
+	if ((downCount%2) === 0)
 	{
-		PLAYER_POS.row++;
-		if ((downCount%2) === 0)
-		{
-			SERGEY = sergeydown1;
-		}
-		
-		else
-		{
-			SERGEY = sergeydown2;
-		}
-		
-		downCount++;
-		
-		setTimeout(moveDown, 1000);
+		SERGEY = sergeydown1;
 	}
+	
+	else
+	{
+		SERGEY = sergeydown2;
+	}
+	
+	downCount++;
 }
 	
 function moveLeft()
 {
-	if (GAME_ARRAY[PLAYER_POS.row][PLAYER_POS.col-1] === 1)
+	PLAYER_POS.col--;
+	if ((leftCount%2) === 0)
 	{
-		PLAYER_POS.col--;
-		if ((leftCount%2) === 0)
-		{
-			SERGEY = sergeyleft1;
-		}
-		
-		else
-		{
-			SERGEY = sergeyleft2;
-		}
-		
-		leftCount++;
-		
-		setTimeout(moveLeft, 1000);
+		SERGEY = sergeyleft1;
 	}
+	
+	else
+	{
+		SERGEY = sergeyleft2;
+	}
+	
+	leftCount++;
 }
 
 function moveRight()
 {
-	if (GAME_ARRAY[PLAYER_POS.row][PLAYER_POS.col+1] === 1)
+	PLAYER_POS.col++;
+	if ((rightCount%2) === 0)
 	{
-		PLAYER_POS.col++;
-		if ((rightCount%2) === 0)
+		SERGEY = sergeyright1;
+	}
+	
+	else
+	{
+		SERGEY = sergeyright2;
+	}
+	
+	rightCount++;
+}
+
+function move()
+{
+	while (NUM_STEPS > 0)
+	{
+		switch (GAME_ARRAY[PLAYER_POS.row][GAME_ARRAY[col])
 		{
-			SERGEY = sergeyright1;
-		}
-		
-		else
-		{
-			SERGEY = sergeyright2;
-		}
-		
-		rightCount++;
-		
-		
-		setTimeout(moveRight, 1000);
+			case 1:
+				setTimeout(moveDown, 1000);
+				break;
+			case 2:
+				setTimeout(moveLeft, 1000);
+				break;
+			case 3:
+				setTimeout(moveRight, 1000);
+				break;
+			case 4:
+				setTimeout(moveUp, 1000);
+				break;
+			case 5:
+				var random = Math.floor(Math.random()*2)+1;
+				if (random === 1)
+				{
+					moveUp();
+				}
+				
+				else
+				{
+					moveLeft();
+				}
+				setTimeout(moveDown, 1000);
+				break;
+			case 6:
+				setTimeout(moveDown, 1000);
+				break;
+		}	
 	}
 }
 	// if (e === 38)
@@ -161,5 +178,91 @@ function moveRight()
 			
 			// rightCount++;
 		// }
+	// }
+// }
+
+
+// function moveUp()
+// {
+	// if (GAME_ARRAY[PLAYER_POS.row-1][PLAYER_POS.col] === 1)
+	// {
+		// PLAYER_POS.row--;
+		// if ((upCount%2) === 0)
+		// {
+			// SERGEY = sergeyup1;
+		// }
+		
+		// else
+		// {
+			// SERGEY = sergeyup2;
+		// }
+		
+		// upCount++;
+		
+		// setTimeout(moveUp, 1000);
+	// }
+// }
+		
+// function moveDown()
+// {
+	// if (GAME_ARRAY[PLAYER_POS.row+1][PLAYER_POS.col] === 1)
+	// {
+		// PLAYER_POS.row++;
+		// if ((downCount%2) === 0)
+		// {
+			// SERGEY = sergeydown1;
+		// }
+		
+		// else
+		// {
+			// SERGEY = sergeydown2;
+		// }
+		
+		// downCount++;
+		
+		// setTimeout(moveDown, 1000);
+	// }
+// }
+	
+// function moveLeft()
+// {
+	// if (GAME_ARRAY[PLAYER_POS.row][PLAYER_POS.col-1] === 1)
+	// {
+		// PLAYER_POS.col--;
+		// if ((leftCount%2) === 0)
+		// {
+			// SERGEY = sergeyleft1;
+		// }
+		
+		// else
+		// {
+			// SERGEY = sergeyleft2;
+		// }
+		
+		// leftCount++;
+		
+		// setTimeout(moveLeft, 1000);
+	// }
+// }
+
+// function moveRight()
+// {
+	// if (GAME_ARRAY[PLAYER_POS.row][PLAYER_POS.col+1] === 1)
+	// {
+		// PLAYER_POS.col++;
+		// if ((rightCount%2) === 0)
+		// {
+			// SERGEY = sergeyright1;
+		// }
+		
+		// else
+		// {
+			// SERGEY = sergeyright2;
+		// }
+		
+		// rightCount++;
+		
+		
+		// setTimeout(moveRight, 1000);
 	// }
 // }
