72d2c9dc152150c0bee7d065bd9de6b8bcf08132
lju <lju@andrew.cmu.edu>
2013-02-28 15:39:42 -0800

    small comment delete

--- static/src1/player.js
+++ static/src1/player.js
@@ -4,8 +4,6 @@ function checkForKeys()
 {
 	var topping;
 	document.onkeyup = function() {
-		// var toppingTempName = "topping" + String(TOPPING_NUM);
-		console.log("something happening");
 		topping = new Topping();
 		TOPPING_ARRAY.push(topping);
 		TOPPING_NUM++;
